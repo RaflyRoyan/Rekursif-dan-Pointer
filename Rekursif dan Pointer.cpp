996bcb1d91d09b5de9e4b331964e4b59bc3c1b79
RaflyRoyan <91069208+RaflyRoyan@users.noreply.github.com>
2021-09-21 08:00:55 +0700

    Add files via upload

---
+++ Rekursif dan Pointer.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int pangkat(int x, int y){
+	if(y == 0){
+		return 1;
+	}
+	else{
+		return x * pangkat(x,y-1);
+	}
+}
+main(){
+	int x, y;
+	
+	printf("Bilangan yang dipangkatkan : ");
+	scanf("%d", &x);
+	printf("Pangkat Bilangan : ");
+	scanf("%d", &y);
+	printf("Hasil Bilangan %d pangkat %d adalah %d\n", x, y, pangkat(x,y));
+	printf("Hasil bilangan pangkat = %d\n", pangkat(x,y));
+	
+	int i = pangkat(x,y);
+	int *ptr;
+	int **pptr;
+	
+	ptr = &i;
+	pptr = &ptr;
+	
+	printf("Slot memori bilangan pangkat = %d\n", *ptr);
+	printf("Slot memori bilangan pangkat menggunakan double pointer = %d\n", **pptr);
+}
+
